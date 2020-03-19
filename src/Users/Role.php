215e49c012a151c29d36d561c3b474ac8de141aa
Andrey Gladyshev <andrey@uchi.pro>
2020-03-19 12:56:16 +0400

    feat(users): добавить возможность быстрого создания объекта роли

--- src/Users/Role.php
+++ src/Users/Role.php
@@ -13,4 +13,18 @@ class Role
      * @var string
      */
     public $title;
+
+    /**
+     * @param $id
+     * @param $title
+     *
+     * @return Role
+     */
+    public static function create($id, $title)
+    {
+        $role = new self();
+        $role->id = $id;
+        $role->title = $title;
+        return $role;
+    }
 }
