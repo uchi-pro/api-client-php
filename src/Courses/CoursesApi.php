7c75d4d53e14f29c7e84775a4fd9ba5114908232
Andrey Gladyshev <andrey@uchipro.ru>
2026-08-26 12:08:03 +0400

    fix(courses): проверять наличие description у урока перед проверкой интерактивного контента

--- src/Courses/CoursesApi.php
+++ src/Courses/CoursesApi.php
@@ -419,8 +419,10 @@ final class CoursesApi
             }
         }
 
-        if (!$courseFeatures->interactive && $this->checkForInteractiveContent($lesson['description'])) {
-            $courseFeatures->interactive = true;
+        if (!$courseFeatures->interactive) {
+            if (isset($lesson['description']) && $this->checkForInteractiveContent($lesson['description'])) {
+                $courseFeatures->interactive = true;
+            }
         }
 
         if (!empty($lesson['resources']) && is_array($lesson['resources'])) {
